5d92e31d099caf3a14f0a61d49ea54e6bea20564
Rajesh Pal <rajesh.zestard@gmail.com>
2023-10-19 15:55:17 +0530

    Demo on 19Oct

---
+++ assets/js/scripts.js
@@ -0,0 +1,29 @@
+var $ = jQuery.noConflict();
+
+/* Script on ready
+------------------------------------------------------------------------------*/
+$(() => {
+	/* Do jQuery stuff when DOM is ready */
+});
+
+/* Script on load
+------------------------------------------------------------------------------*/
+window.onload = () => {
+	/* Do jQuery stuff when DOM is fully loaded, including all frames, objects and images */
+};
+
+/* Script on scroll
+------------------------------------------------------------------------------*/
+window.onscroll = () => {
+	/* Do jQuery stuff on Scroll */
+};
+
+/* Script on resize
+------------------------------------------------------------------------------*/
+
+window.onresize = (event) => {
+	/* Do jQuery stuff on resize */
+};
+
+/* Script all functions
+------------------------------------------------------------------------------*/
